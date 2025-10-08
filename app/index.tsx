64eb8b82f6127f9d81afa535a6e80e386a4dc9b2
roldanmatias441 <roldanmatias441@gmail.com>
2025-10-08 15:30:19 +0000

    ✨ feat(index):Se agrega un nuevo boton y se le agrega una alerta para cada boton.

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import { ImageBackground, View } from "react-native";
-import "../global.css";
+import Button from "../components/ui/Button";
 import CustomText from "../components/ui/CustomText";
 import InputField from "../components/ui/InputField";
-import Button from "../components/ui/Button";
 import LinkText from "../components/ui/LinkText";
+import "../global.css";
 
 export default function Index() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [haIniciadoSesion, setHaIniciadoSesion] = useState(false);
+  const [seHaRegistrado, setSeHaRegistrado] = useState(false);
 
-  const handleSignUp = () => {
-    console.log("Registrar con:", email, password);
-  };
+  function handleLogin() {
+    if (!haIniciadoSesion) {
+      alert("Logeando");
+      setHaIniciadoSesion(true);
+    }
+  }
+
+  function handleRegister() {
+    if (!seHaRegistrado) {
+      alert("Registrando");
+      setSeHaRegistrado(true);
+    }
+  }
 
   return (
     <ImageBackground
@@ -21,16 +33,33 @@ export default function Index() {
       resizeMode="cover"
     >
       <View className="bg-black/50 p-10 rounded-2xl w-80 items-center shadow-xl">
-        <CustomText variant="large" dark={true}>Registrarse</CustomText>
+        <CustomText variant="large" dark={true}>Bienvenido</CustomText>
 
-        <InputField placeholder="Correo electrónico" value={email} onChangeText={setEmail} />
-        <InputField placeholder="Contraseña" value={password} secureTextEntry onChangeText={setPassword} />
+        <InputField 
+          placeholder="Correo electrónico" 
+          value={email} 
+          onChangeText={setEmail} 
+        />
+        <InputField 
+          placeholder="Contraseña" 
+          value={password} 
+          secureTextEntry 
+          onChangeText={setPassword} 
+        />
 
-        <Button title="Crear cuenta" onPress={handleSignUp} />
+        {/* Botón de Login */}
+        <Button title="Iniciar sesión" onPress={handleLogin} />
+
+        {/* Botón de Registrar */}
+        <Button title="Registrarse" onPress={handleRegister} />
 
         <View className="flex-row justify-between w-full">
-          <CustomText variant="small" dark={true}>¿Ya tienes una cuenta?</CustomText>
-          <LinkText text="Inicia sesión" dark={false} onPress={() => console.log("Navegar a login")} />
+          <CustomText variant="small" dark={true}>¿Olvidaste tu contraseña?</CustomText>
+          <LinkText 
+            text="Recuperar" 
+            dark={false} 
+            onPress={() => console.log("Navegar a recuperar")} 
+          />
         </View>
       </View>
     </ImageBackground>
